121202994831ba148d8b71a270598220c554ac57
Enragedtoast <edwardsjtimothy@gmail.com>
2020-01-30 17:07:21 -0600

    added basic  html templates for home and about pages

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+      "author": "Tim",
+      "title": "First Post",
+      "content": "The dog ran up the hill.",
+      "data_posted": "January 30th 2020"
+    },
+    {
+      "author": "Zorp",
+      "title": "Second Post",
+      "content": "The Fox ran up the hill.",
+      "data_posted": "January 30th 2020"
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html")
