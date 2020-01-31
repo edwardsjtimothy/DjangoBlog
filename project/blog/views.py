d6ec736328654abaf07e8ce053a6793f22ac6a3f
Enragedtoast <edwardsjtimothy@gmail.com>
2020-01-30 18:22:44 -0600

    added base.html template

--- project/blog/views.py
+++ project/blog/views.py
@@ -6,13 +6,13 @@ posts = [
       "author": "Tim",
       "title": "First Post",
       "content": "The dog ran up the hill.",
-      "data_posted": "January 30th 2020"
+      "date_posted": "January 30th 2020"
     },
     {
       "author": "Zorp",
       "title": "Second Post",
       "content": "The Fox ran up the hill.",
-      "data_posted": "January 30th 2020"
+      "date_posted": "January 30th 2020"
     }
 ]
 
@@ -24,4 +24,4 @@ def home(request):
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "About"})
